bd447d3faa2ce498f8188199f75b973aa8317357
Shubham_Baghel <shubhambaghel34@gmail.com>
2023-07-16 16:12:33 +0530

    updated code with comments

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-const parent = React.createElement("h1", {id:'parent'}, [
-    React.createElement('div',{},[
-        React.createElement('h1',{},"h1 tag"),
-        React.createElement('h2',{},"h2 tag"),
 
-    ]),
-    React.createElement('div',{},[
-        React.createElement('h1',{},"h1 tag"),
-        React.createElement('h2',{},"h2 tag"),
-
-    ]),
+const parent = React.createElement("h1", { id: "parent" }, [
+//third paramenter is attributes for tags
+    React.createElement("div", {}, [
+    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h2", {}, "h2 tag"),
+  ]),
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h2", {}, "h2 tag"),
+  ]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
+console.log(parent);//object
 root.render(parent);
