936d8e9a833f54c8b24e80530b320faa2367df4a
Shubham_Baghel <shubhambaghel34@gmail.com>
2023-07-16 16:11:16 +0530

    initial code

--- App.js
+++ App.js
@@ -1,4 +1,15 @@
-const heading = React.createElement("h1", {}, "Hello for h1 Tag");
+const parent = React.createElement("h1", {id:'parent'}, [
+    React.createElement('div',{},[
+        React.createElement('h1',{},"h1 tag"),
+        React.createElement('h2',{},"h2 tag"),
+
+    ]),
+    React.createElement('div',{},[
+        React.createElement('h1',{},"h1 tag"),
+        React.createElement('h2',{},"h2 tag"),
+
+    ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading);
-root.render(heading);
+console.log(parent);
+root.render(parent);
