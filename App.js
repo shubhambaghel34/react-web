63cd440d7435b287017984c674e1a4be90d8aea1
Shubham_Baghel <shubhambaghel34@gmail.com>
2023-07-16 19:33:15 +0530

    added code function component,component composition

--- App.js
+++ App.js
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom/client";
 
 //React Element using JSX
 
-//(HTML/XML like syntax) where code transpiled before it reaches to JS engine,Parcel=>Babel convert code which understand by JS engine 
-const jsxheading=(<h1>Hello from React js with JSX</h1>)
-
+//(HTML/XML like syntax) where code transpiled before it reaches to JS engine,Parcel=>Babel convert code which understand by JS engine
+const jsxheading = <h1>Hello from React js with JSX</h1>;
 
 //JSX-->Babel transpile to-->React.createElement==>Reactelement.JS Object => HTMLelement
-const root=ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheading);
+//React Element
+const headings = (<h1>Hello from React js with JSX</h1>);
+
+
+//This is component composition
+const HeadingComponent = () => (
+   <h1>Hello from Heading  Component</h1>
+);
+
+const TitleComponent = () => (
+  <div>
+    <HeadingComponent />
+    <h1>Hello from Title Component </h1>;
+  </div>
+);
+
+root.render(<TitleComponent />);
