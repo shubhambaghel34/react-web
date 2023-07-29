fb92d6b379e2ab3d19250d6983f6d8f3f9de3ad0
Shubham_Baghel <shubhambaghel34@gmail.com>
2023-07-29 18:37:31 +0530

    added code for class component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,14 @@
+import User from "../user/User";
+import UserClass from "../user/UserClass";
+
 const AboutUs=()=>{
     return(
         <div>
             <span>Hello from About us page</span>
+            <User name="shubham" contact="11111" location="PUNE"/>
+            <span>
+            <UserClass name="shubhamB" contact="1111122" location="PUNE-PCMC"/>
+        </span>
         </div>
     )
 }
---
+++ src/components/user/UserClass.js
@@ -0,0 +1,20 @@
+import React from "react";
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+
+  render() {
+    const {name,location,contact}=this.props;
+    return (
+      <div className="userclass-card">
+        <h2>Name:{name}</h2>
+        <h3>Location:{location}</h3>
+        <h4>Contact:{contact}</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
