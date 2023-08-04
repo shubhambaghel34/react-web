5b8a058a513977dbb01edb6c498ceadaef8e61cf
Shubham_Baghel <shubhambaghel34@gmail.com>
2023-08-04 09:04:19 +0530

    added code for class component_updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,37 +1,25 @@
-import {Component} from "react";
+import { Component } from "react";
 import User from "../user/User";
 import UserClass from "../user/UserClass";
 
-
-class AboutUs extends Component{
- constructor(props){
+class AboutUs extends Component {
+  constructor(props) {
     super(props);
-    
- }
-    
- componentDidMount(){
-    
   }
-  
-    render(){
-       
-        return(
-                 <div className="about-us">
-                    <span>Hello from About us page</span>
-                  
-                    <span>
-                   
-                    <UserClass name="Elon Musk" contact="1221122" location="DXB"/>
-                </span>
-                </div>
-                
-        )
-    }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="about-us">
+        <span>Hello from About us page</span>
+        <span>
+          <UserClass name="Elon Musk" contact="1221122" location="DXB" />
+        </span>
+      </div>
+    );
+  }
 }
-
-
-
-
 
 // const AboutUs=()=>{
 //     return(
